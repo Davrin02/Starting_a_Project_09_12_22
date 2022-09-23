518db81028a485ccebae3773edd667691657af06
david.murcia <david.murcia@ufl.edu>
2022-09-23 18:24:47 -0400

    adding repository sys_info.sh

---
+++ sys_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Program to output a system information page
+echo "<html>"
+echo "  <head>"
+echo "       <title>System Information Report Page Title</title>"
+echo "  </head>"
+echo "  <body>"
+echo "    body_body_website"
+echo "  </body>"
+echo "</html>"
